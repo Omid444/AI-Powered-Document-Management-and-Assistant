48bbbf21dc276ff5015cafd401c86789e77efb72
Omid444 <86495623+Omid444@users.noreply.github.com>
2025-07-21 17:10:21 +0200

    css file added. BASE_DIR created in app.py to simplify routing

--- app/api/app.py
+++ app/api/app.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pathlib import Path
 from fastapi import FastAPI
 import models.schemas
 import services.auth
@@ -13,8 +14,9 @@ import uvicorn
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="../../static"), name="static")
-templates = Jinja2Templates(directory="../../templates")
+BASE_DIR = Path(__file__).resolve().parent.parent.parent #Back to root of project
+app.mount("/static", StaticFiles(directory=BASE_DIR/"static"), name="static")
+templates = Jinja2Templates(directory=BASE_DIR/"templates")
 
 def get_db():
     """
@@ -72,5 +74,7 @@ def signup(user: models.schemas.UserCreate, db: Session = Depends(get_db)):
 # async def read_item(skip: int = 0, limit: int = 10):
 #     return fake_items_db[skip : skip + limit]
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+# uvicorn app.api.app:app --reload
